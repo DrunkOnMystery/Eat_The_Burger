a136fde9f527f50684a48a4e527308c3cfbdb4a8
Ben Brumby <bbrumby00@gmail.com>
2020-10-20 20:17:14 -0400

    JAWSDB pt 3

--- config/connection.js
+++ config/connection.js
@@ -14,10 +14,10 @@ if (process.env.JAWSDB_URL) {
 }
 else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "hacktheplanet",
-    database: "todoagain_db"
+    host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "x1nvsrtgu0kjfq6c",
+    password: "arx0dslivlp19uuu",
+    database: "xe02cvtwatxmhx4x"
   });
 };
 // Make connection.
