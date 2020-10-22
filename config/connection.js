36ec3533d8a08ad5b988e64fc6686c9c44db2546
Ben Brumby <bbrumby00@gmail.com>
2020-10-22 09:39:59 -0400

    fixed connection points

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Angstboy8.com",
-  database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-  connection = mysql.createConnection({
-    host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "kt3u8qjnf8av3plw",
-    password: "s43306h6s2q8dnp4",
-    database: "zw2ufrdso2ul3ccs"
+    connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Angstboy8.com",
+    database: "burgers_db"
   });
+
 };
 // Make connection.
 connection.connect(function(err) {
